0042aef5e7120355b5103d3da66cc6cea1323dc7
Franco Zelione Lenzi <francozelione796@gmail.com>
2023-05-30 15:14:32 -0300

    Ultimo commit

--- src/components/Header/components/CartWidget/CartWidget.jsx
+++ src/components/Header/components/CartWidget/CartWidget.jsx
@@ -8,7 +8,11 @@ const CartWidget = () =>{
             <button className="botonCarrito">
                 <img src="/img/carrito.png" alt="" className="imagenCarrito"/>
             </button>
+            <div className="itemsCarrito">
+                <p className="numeroCarrito">0</p>
+            </div>
         </section>
+
     )
 
 }
